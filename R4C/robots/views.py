4116ef1a62f30e8d9a6ec2a37801622648a50560
Emin <bagirov3min@gmail.com>
2023-10-01 05:28:58 +0530

    Создан API-endpoint в виде функции представления, принимающий и обрабатывающий информацию в формате JSON. В результате запросов на этот endpoint в базе создается запись в соответствии с заданной валидацией входных данных.

--- R4C/robots/views.py
+++ R4C/robots/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .forms import RobotForm
 
-# Create your views here.
+
+@csrf_exempt
+def add_robot(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        form = RobotForm(data)
+
+        if form.is_valid():
+            robot = form.save(commit=False)
+            robot_serial = f"{robot.model}-{robot.version}"
+            robot.serial = robot_serial
+            robot.save()
+            return JsonResponse({'message': 'Robot created successfully'}, status=201)
+        else:
+            errors = form.errors.as_json()
+            return JsonResponse({'errors': errors}, status=400)
+    else:
+        return JsonResponse({'error': 'Invalid method'}, status=405)
