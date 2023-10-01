baa772215e9d15841177049295af820ea1ba8774
Emin <bagirov3min@gmail.com>
2023-10-01 05:40:31 +0530

    Добавлена новая функция представления generate_excel, доступная по GET-запросу на http://localhost:8000/generate_excel/. Функция формирует Excel-файл, содержащий сводку по суммарным показателям производства роботов за последнюю неделю. Файл включает информацию о модели, версии и количестве произведенных роботов. Каждая модель имеет свою отдельную страницу в файле.

--- R4C/robots/urls.py
+++ R4C/robots/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
+
+from R4C import settings
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('add_robot/', views.add_robot, name='add_robot'),
+    path('generate_excel/', views.generate_excel, name='generate_excel'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- R4C/robots/views.py
+++ R4C/robots/views.py
@@ -1,7 +1,15 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-import json
+from django.conf import settings
+from django.db.models import Count
 from .forms import RobotForm
+from .models import Robot
+from datetime import datetime, timedelta
+import json
+import os
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+import pandas as pd
 
 
 @csrf_exempt
@@ -21,3 +29,38 @@ def add_robot(request):
             return JsonResponse({'errors': errors}, status=400)
     else:
         return JsonResponse({'error': 'Invalid method'}, status=405)
+
+
+def generate_excel(request):
+    start_date = datetime(2023, 9, 25, 0, 0)
+    current_date = datetime.now()
+    while current_date >= start_date + timedelta(days=7):
+        start_date += timedelta(days=7)
+    end_date = datetime.now()
+
+    wb = Workbook()
+    models = Robot.objects.values_list("model", flat=True).distinct()
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = f"summary_{current_time}.xlsx"
+    data_found = False
+
+    for model in models:
+        data = Robot.objects.filter(
+            model=model,
+            created__range=(start_date, end_date)
+        ).values("model", "version").annotate(total=Count("id"))
+
+        df = pd.DataFrame.from_records(data)
+
+        if not df.empty:
+            data_found = True
+            ws = wb.create_sheet(title=model)
+            for row in dataframe_to_rows(df, index=False, header=True):
+                ws.append(row)
+    if data_found:
+        default_sheet = wb.get_sheet_by_name("Sheet")
+        wb.remove(default_sheet)
+        file_path = os.path.join(settings.MEDIA_ROOT, 'excel_files', file_name)
+        wb.save(file_path)
+        return JsonResponse({"file_url": os.path.join(settings.MEDIA_URL, 'excel_files', file_name)})
+    return JsonResponse({'errors': "На этой неделе еще не было создано роботов"}, status=400)
